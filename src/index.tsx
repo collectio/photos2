1f6f1fabff7404f3435bf2a0ca1a2ad299339bbf
deguchi <deguchik@gmail.com>
2021-07-12 10:59:18 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
