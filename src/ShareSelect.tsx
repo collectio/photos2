a9a9c4a7a922938ae65471c43ba59eea80aefe5e
deguchi <deguchik@gmail.com>
2021-07-18 12:24:44 +0900

    update

--- src/ShareSelect.tsx
+++ src/ShareSelect.tsx
@@ -12,48 +12,46 @@ import { AlbumType, PhotoType, GameType } from './@types'
 const ShareSelect: React.VFC = (props: any) => {
     const albums: AlbumType[] = useSelector(selectAlbums)
     const defaultAlbum: unknown = null
-    const [state, setState] = useState({
-        album: defaultAlbum as AlbumType,
-        selectedImageIndex: [] as number[],
-        selectDisabled: false,
-    })
+    const [album, setAlbum] = useState(defaultAlbum as AlbumType)
+    const [selectedIndexes, setSelectedIndexes] = useState([] as number[])
+    const [selectDisabled, setSelectDisabled] = useState(false)
 
     let { id } = useParams<{ id: string }>()
     useEffect(() => {
         scrollTo(0, 0)
-        if (!state.album) {
+        if (!album) {
             const alb = albums.filter((a) => a.id === id)[0]
-            if (alb) setState({...state, album: alb})
+            if (alb) setAlbum(alb)
         }
     })
 
     const select = (index: number) => {
-        if (state.selectedImageIndex.indexOf(index) === -1) {
-            if (state.selectDisabled === false) {
-                state.selectedImageIndex.push(index)
+        console.log(index)
+        if (selectedIndexes.indexOf(index) === -1) {
+            if (selectDisabled === false) {
+                selectedIndexes.push(index)
+                setSelectedIndexes(selectedIndexes)
             }
         } else {
-            state.selectedImageIndex.splice(state.selectedImageIndex.indexOf(index), 1)
-            setState({...state, selectDisabled: false})
+            selectedIndexes.splice(selectedIndexes.indexOf(index), 1)
+            setSelectDisabled(false)
         }
-        if (state.selectedImageIndex.length >= 4) {
-            setState({...state, selectDisabled: true})
+        if (selectedIndexes.length >= 4) {
+            setSelectDisabled(true)
         }
-        setState(state)
     }
 
     const share = () => {
-        if (state.selectedImageIndex.length === 0) return alert('写真を選択してください')
-        const photos = state.album.photos.filter((photo: PhotoType, index: number) => state.selectedImageIndex.indexOf(index) > -1)
+        if (selectedIndexes.length === 0) return alert('写真を選択してください')
+        const photos = album.photos.filter((photo: PhotoType, index: number) => selectedIndexes.indexOf(index) > -1)
         props.history.push({
             pathname: '/share',
-            state: { photos: photos, album: state.album }
+            state: { photos: photos, album: album }
         })
     }
 
-    if (!state.album) return null
-    const album = state.album
-    return (<div id="albumSelect">
+    if (!album) return null
+    return (<div id="shareSelect">
         <nav>
             <Link to={{pathname: `/album/${album.id}`}}>
                 <img className="logo" src="/back.svg" alt="戻る" />
@@ -64,8 +62,8 @@ const ShareSelect: React.VFC = (props: any) => {
         <div className="album">
             <div className="photos">
                 {album.photos.map((photo: PhotoType, index: number) => {
-                    return (<div key={photo.image} className={'photo' + (state.selectDisabled ? ' disabled' : '')} onClick={() => select(index)} style={{ backgroundImage: `url(${photo.image})` }}>
-                        <span className={'select' + (state.selectedImageIndex.indexOf(index) > -1 ? ' selected' : '')}></span>
+                    return (<div key={photo.image} className={'photo' + (selectDisabled ? ' disabled' : '')} onClick={() => select(index)} style={{ backgroundImage: `url(${photo.image})` }}>
+                        <span className={'select' + (selectedIndexes.indexOf(index) > -1 ? ' selected' : '')}></span>
                     </div>);
                 })}
             </div>
