6c5abb3905ba55ca09073b79612566930a909b47
deguchi <deguchik@gmail.com>
2021-07-28 10:08:09 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
     https: true
   },
   plugins: [reactRefresh()],
-  publicDir: './src/assets/'
+  publicDir: './src/assets/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      }
+    }
+  }
 })
