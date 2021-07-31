adeb3f9a4775f922b8c2a8a93f8a2dcb013046b6
deguchi <deguchik@gmail.com>
2021-07-31 14:39:39 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -173,7 +173,7 @@ const browserImageRotationSupport = () => {
     return imgTag.style.imageOrientation !== undefined;
 }
 
-const degreeFromExif = async (image: any): number => {
+const degreeFromExif = async (image: any): Promise<number> => {
     const orientation = await exifr.orientation(image)
     let degree = 0
     switch (orientation) {
@@ -192,24 +192,12 @@ const degreeFromExif = async (image: any): number => {
 
 
 const drawRotated = (image: any, canvas: any, context: any, degrees: number) => {
-    context.clearRect(0,0,canvas.width,canvas.height);
-
-    // save the unrotated context of the canvas so we can restore it later
-    // the alternative is to untranslate & unrotate after drawing
-    context.save();
-
-    // move to the center of the canvas
-    context.translate(canvas.width / 2, canvas.height / 2);
-
-    // rotate the canvas to the specified degrees
-    context.rotate(degrees * Math.PI / 180);
-
-    // draw the image
-    // since the context is rotated, the image will be rotated also
-    context.drawImage(image, -image.width / 2, -image.width / 2);
-
-    // we’re done with the rotating so restore the unrotated context
-    context.restore();
+    context.clearRect(0,0,canvas.width,canvas.height)
+    context.save()
+    context.translate(canvas.width / 2, canvas.height / 2)
+    context.rotate(degrees * Math.PI / 180)
+    context.drawImage(image, -image.width / 2, -image.width / 2)
+    context.restore()
 }
 
 
