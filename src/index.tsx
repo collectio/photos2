d0819e1867ffb772584cdc9f0945f1e99be46b8c
deguchi <deguchik@gmail.com>
2021-09-04 11:07:35 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,23 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 // import * as serviceWorker from './serviceWorker'
 
+function getQueryString() {
+  var params = {}
+  location.search.substr(1).split('&').map(function(param) {
+      var pairs = param.split('=');
+      // @ts-ignore
+      params[pairs[0]] = decodeURIComponent(pairs[1]);
+  });
+  return params;    
+}
+
+declare let window: any;
+
+const params: any = getQueryString();
+if (params['cordova'] === 'true') {
+  window.cordova = true
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
