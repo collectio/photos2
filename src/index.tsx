65e6856ff01db301fa3f7374f1e4b358d08b33f2
deguchi <deguchik@gmail.com>
2021-09-05 08:06:42 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -32,6 +32,7 @@ const start = () => {
 
 const params: any = getQueryString();
 if (params['cordova'] === 'true') {
+  alert('アプリのテストバージョンです')
   document.addEventListener('deviceready', start, false)
 } else {
   start()
