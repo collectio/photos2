2e5f3fb73fb8a4ecdd0ef7b5dc9f61e69fdd6529
deguchi <deguchik@gmail.com>
2021-10-02 11:11:06 +0900

    update

--- src/Share.tsx
+++ src/Share.tsx
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom'
 import { AlbumType, PhotoType, GameType } from './@types/index'
 
 interface Share {
-    textArea: any
 }
 interface Props {
 }
@@ -19,15 +18,14 @@ interface ShareData {
     files?: File[];
 }
 
-const convertFile = async (url: string) => {
+const convertFile = async (url: string, index: number) => {
     const blob = await fetch(url).then(r => r.blob())
     // console.log(blob.type)
-    return new File([blob], 'test.jpg',{ type: blob.type })
+    return new File([blob], `test${index}.jpg`,{ type: blob.type })
 }
 
 
 const Share: React.VFC = (props: any) => {
-    const textArea = useRef(null)
     const [state, setState] = useState({
         album: {} as AlbumType,
         photos: []
@@ -51,21 +49,16 @@ const Share: React.VFC = (props: any) => {
     }, [state.album, state.photos])
 
 
-    const share = async (text: string) => {
+    const share = async () => {
         const files: File[] = []
         for (const photo of state.photos) {
             // @ts-ignore
-            const file = await convertFile(photo.image)
+            const file = await convertFile(photo.image, Math.floor(Math.random() * 10))
             files.push(file)
         }
-        const titles: string[] = []
-        state.album.games.forEach((game) => {
-            if (titles.indexOf(game.title) === -1) titles.push('#' + game.title)
-        })
-        text += '\n' + titles.join(' ')
         if (navigator.share) {
             navigator.share({
-                text: text,
+                // text: text,
                 url: 'https://collectio.jp/',
                 files: files
             } as ShareData).then(() => {
@@ -76,12 +69,7 @@ const Share: React.VFC = (props: any) => {
         } else {
             alert('このブラウザではシェア機能が使えません。\n最新のSafari, Chromeをお使いください。')
         }
-    }
-    
-    const onClick = () => {
-        // @ts-ignore
-        share(textArea.current.value)
-    }
+    }   
 
     if (state.album === null) return null
     return (<div id="share">
@@ -96,8 +84,7 @@ const Share: React.VFC = (props: any) => {
                 </div>);
             })}
         </div>
-        <textarea name="comment" id="comment" ref={textArea} placeholder="ゲームを遊んだ感想など"></textarea>
-        <button onClick={onClick}>
+       <button onClick={share}>
             <svg xmlns="http://www.w3.org/2000/svg" width="19.067" height="24.245" viewBox="0 0 19.067 24.245"><path d="M.005-4.834a.855.855,0,0,0,.849-.838V-16.7L.79-18.315l.73.763,1.622,1.74a.784.784,0,0,0,.58.258.751.751,0,0,0,.784-.763.75.75,0,0,0-.247-.559l-3.631-3.5a.838.838,0,0,0-.623-.29.838.838,0,0,0-.623.29l-3.631,3.5a.76.76,0,0,0-.258.559.748.748,0,0,0,.773.763.79.79,0,0,0,.591-.258l1.633-1.74.73-.763L-.854-16.7V-5.672A.865.865,0,0,0,.005-4.834ZM-6.161,3.577H6.161c2.245,0,3.373-1.117,3.373-3.33V-10.474c0-2.213-1.128-3.33-3.373-3.33h-3v1.729H6.128a1.519,1.519,0,0,1,1.676,1.7V.15a1.519,1.519,0,0,1-1.676,1.7H-6.139A1.507,1.507,0,0,1-7.8.15V-10.377a1.507,1.507,0,0,1,1.665-1.7h2.976V-13.8h-3c-2.245,0-3.373,1.117-3.373,3.33V.247C-9.534,2.46-8.406,3.577-6.161,3.577Z" transform="translate(9.534 20.668)" /></svg>
             共有する
         </button>
